5adbd9a462f504ff7b37772751cd3b9d70e4f344
Andre <aspbarruda@gmail.com>
2022-10-23 20:40:09 -0300

    Added button to change grid size

--- scripts.js
+++ scripts.js
@@ -1,9 +1,33 @@
 const container = document.getElementById('container');
 
-for (let i = 0; i < 16; i++) {
+function updateGrid(n) {
+    for (let i = 0; i < n; i++) {
+        let subcontainer = document.createElement('div');
+        subcontainer.classList = 'line';
+        for (let j = 0; j < n; j++) {
+            let div = document.createElement('div');
+            div.classList = 'square';
+            subcontainer.appendChild(div);
+        }
+        container.appendChild(subcontainer);
+    }
+}
+
+function cleanGrid() {
+    let grid = document.querySelectorAll('.line');
+    grid.forEach(element => {
+        element.remove();        
+    });
+}
+
+
+const button = document.getElementById('button');
+let n = 16;
+
+for (let i = 0; i < n; i++) {
     let subcontainer = document.createElement('div');
     subcontainer.classList = 'line';
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < n; j++) {
         let div = document.createElement('div');
         div.classList = 'square';
         subcontainer.appendChild(div);
@@ -11,7 +35,21 @@ for (let i = 0; i < 16; i++) {
     container.appendChild(subcontainer);
 }
 
-const squares = document.querySelectorAll('.square');
+let squares = document.querySelectorAll('.square');
+
+button.addEventListener('click', () => {
+    do {
+        n = prompt('Enter number of squares on one side: ');
+    } while(isNaN(n) || n < 1 || n > 100);
+    cleanGrid();
+    updateGrid(n);
+    squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.classList.toggle('color');
+        })
+    });
+})
 
 squares.forEach(square => {
     square.addEventListener('mouseover', () => {
